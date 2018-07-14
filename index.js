9eb70f54fd2a717828cbf793aabf8b918b133b24
黄正 <zheng.huang@flaginfo.com.cn>
2018-07-14 20:50:21 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var loaderUtils = require('loader-utils');
 
-module.exports = function (source, inputSourceMap) {
-    console.log(inputSourceMap)
-    var opts = loaderUtils.getOptions(this) || {};
+module.exports = function (source) {
+    var opts = Object.assign(
+        {isDebug: process.env.NODE_ENV === 'development'}, //默认isDebug
+        loaderUtils.getOptions(this)
+    );
     return replaceIfDebug(source, opts)
 };
 
 function replaceIfDebug(js, opt) {
-    var isDebug = opt.isDebug !== false; //默认isDebug为true
+    var isDebug = opt.isDebug !== false;
     var reg = /\/\*IFDEBUG([\s\S]+?)FIDEBUG\*\//g;
-    return js.replace(reg, isDebug ? "$1" : "");
+    return js.replace(reg, isDebug ? '$1' : '');
 }
