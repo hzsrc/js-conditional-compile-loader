6e2d331ab03e9aee01adbe7db8daeae3cadcd748
hzsrc <hz8889999>
2020-04-13 10:22:58 +0800

    支持同一环境多种配置

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var loaderUtils = require('loader-utils');
 var replacer = require('./replacer.js')
 
-var options
 
 module.exports = function (source) {
-  if (!options) {
-    options = Object.assign(
-      { isDebug: process.env.NODE_ENV === 'development' }, //默认的isDebug
-      loaderUtils.getOptions(this)
-    );
-  }
-  return replacer.replaceMatched(source, options)
+    var options = loaderUtils.getOptions(this)
+    if (!('isDebug' in options)) {
+        options.isDebug = process.env.NODE_ENV === 'development'; //默认的isDebug
+    }
+    return replacer.replaceMatched(source, options)
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 var replacer = require('../replacer')
+var plugin = require('../index')
 var glob = require('glob')
 var fs = require('fs')
 var path = require('path')
@@ -11,49 +12,61 @@ var resolve = path.resolve.bind(null, __dirname)
 //   })
 // })
 
+
 test('IFDEBUG true', () => {
-    testOne('cases/IFDEBUG.js', { isDebug: true }, /font-size:13px[\s\S]+this.ruleData/)
+    testOne('cases/IFDEBUG.js', {isDebug: true}, /font-size:13px[\s\S]+this.ruleData/)
 })
 
 test('IFDEBUG false', () => {
-    testOne('cases/IFDEBUG.js', { isDebug: false }, /template: ''\s+,\s+watch: \{\s+\}/)
+    testOne('cases/IFDEBUG.js', {isDebug: false}, /template: ''\s+,\s+watch: \{\s+\}/)
 })
 
 test('IFTRUE myFlag=true', () => {
-    testOne('cases/IFTRUE.js', { myFlag: true }, /font-size:13px[\s\S]+this.ruleData/)
+    testOne('cases/IFTRUE.js', {myFlag: true}, /font-size:13px[\s\S]+this.ruleData/)
 })
 test('IFTRUE myFlag=false', () => {
-    testOne('cases/IFTRUE.js', { myFlag: false }, /template: ''\s+,\s+watch: \{\s+\}/)
+    testOne('cases/IFTRUE.js', {myFlag: false}, /template: ''\s+,\s+watch: \{\s+\}/)
 })
 
 
 test('Divided true', () => {
-    testOne('cases/Divided.js', { isDebug: true, myFlag: true }, /font-size:13px[\s\S]+this.ruleData/)
+    testOne('cases/Divided.js', {isDebug: true, myFlag: true}, /font-size:13px[\s\S]+this.ruleData/)
 })
 test('Divided false', () => {
-    testOne('cases/Divided.js', { isDebug: false, myFlag: false }, /template: ''\s+,\s+watch: \{\s+\}/)
+    testOne('cases/Divided.js', {isDebug: false, myFlag: false}, /template: ''\s+,\s+watch: \{\s+\}/)
 })
 
 
 test('performance', () => {
     var start = new Date()
     var src = fs.readFileSync(resolve('cases/perform.txt'), 'utf-8')
-    for (var i = 0; i < 30; i++) {
+    for (var i = 0; i < 100; i++) {
         src = '' + Math.random() + new Date() + src
-        replacer.replaceMatched(src, { isDebug: false, myFlag: false })
-        replacer.replaceMatched(src, { isDebug: true, myFlag: true })
-        replacer.replaceMatched(src, { isDebug: false, myFlag: true })
-        replacer.replaceMatched(src, { isDebug: true, myFlag: false })
+        mockPlugin(src, {isDebug: false, myFlag: false})
+        mockPlugin(src, {isDebug: true, myFlag: true})
+        mockPlugin(src, {isDebug: false, myFlag: true})
+        mockPlugin(src, {isDebug: true, myFlag: false})
     }
     var spent = new Date() - start
     expect(spent).toBeLessThan(1000)
 })
 
+
+test('IFDEBUG default ifDebug', () => {
+    process.env.NODE_ENV = 'development'
+    //expect(process.env.NODE_ENV).toMatch(/development/)
+    testOne('cases/IFDEBUG.js', {}, /font-size:13px[\s\S]+this.ruleData/)
+})
+
 function testOne(file, options, reg, notReg) {
     var src = fs.readFileSync(resolve(file), 'utf-8')
-    var result = replacer.replaceMatched(src, options)
+    var result = mockPlugin(src, options)
     // fs.writeFileSync(resolve('./dist/' + path.basename(file)), result)
 
     expect(result).toMatch(reg)
     expect(result).not.toMatch(replacer._reg)
+}
+
+function mockPlugin(src, option) {
+    return plugin.call({query: option}, src)
 }
